f64fe5fe7de7c2fe73aac3195f4664f5b4ae8705
Mitsunori Kuroda <kuroda@e-kuro.com>
2020-11-25 12:43:41 +0900

    bootstrapが静的ファイルを検知したときに、そのままファイルを返す機能を追加

---
+++ html/app/bootstrap-config.inc.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+// ドキュメントルート(*)
+*/
+define( 'HOME_DIR', '/var/www/e-kuro.com/html/testplay/denryoku-sharing/html/' );
+
+/*
+// ウェブアプリケーションディレクトリ(*)
+*/
+define( 'WEB_APP', '/var/www/e-kuro.com/html/testplay/denryoku-sharing/webapp/');
+
+/*
+// ウェブアプリケーションCONFIGディレクトリ(*)
+*/
+define( 'WEB_APP_CONFIG_DIR', '/var/www/e-kuro.com/html/testplay/denryoku-sharing/webapp/config/');
+
+/*
+// ブートパス認識パラメータ
+*/
+define( 'BOOT_SERVER_PARAM', 'REDIRECT_URL' );
+
+/*
+// PHPディレクトリ(*)
+*/
+define( 'BOOT_PHP_DIR', HOME_DIR . 'sources/');
+
+/*
+// PHP拡張子(*)
+*/
+define( 'BOOT_PHP_EXTENSION', '.php');
+
+/*
+// HTMLディレクトリ(*)
+*/
+define( 'BOOT_HTML_DIR', HOME_DIR . 'templates/');
+
+/*
+// HTML拡張子(*)
+*/
+define( 'BOOT_HTML_EXTENSION', '.html');
+
+/*
+// ファイルパス調整用(*)
+*/
+define( 'BOOT_DELETE_PATH', 'testplay/denryoku-sharing/html/');
+
+/*
+// オリジナルURL(*)
+*/
+define( 'BOOT_ORIGIN_PARAM', 'origin_file');
+
+
--- html/app/bootstrap.php
+++ html/app/bootstrap.php
@@ -1,58 +1,9 @@
 <?php
 
-/*
-// ドキュメントルート(*)
-*/
-define( 'HOME_DIR', '/var/www/e-kuro.com/html/testplay/denryoku-sharing/html/' );
-
-/*
-// ウェブアプリケーションディレクトリ(*)
-*/
-define( 'WEB_APP', '/var/www/e-kuro.com/html/testplay/denryoku-sharing/webapp/');
-
-/*
-// ウェブアプリケーションCONFIGディレクトリ(*)
-*/
-define( 'WEB_APP_CONFIG_DIR', '/var/www/e-kuro.com/html/testplay/denryoku-sharing/webapp/config/');
-
-/*
-// ブートパス認識パラメータ
-*/
-define( 'BOOT_SERVER_PARAM', 'REDIRECT_URL' );
-
-/*
-// PHPディレクトリ(*)
-*/
-if ($_SERVER['REMOTE_ADDR'] == "160.16.69.206") {
-define( 'BOOT_PHP_DIR', HOME_DIR . 'sources_test/');
-} else {
-define( 'BOOT_PHP_DIR', HOME_DIR . 'sources/');
-}
-
-/*
-// PHP拡張子(*)
-*/
-define( 'BOOT_PHP_EXTENSION', '.php');
-
-/*
-// HTMLディレクトリ(*)
-*/
-define( 'BOOT_HTML_DIR', HOME_DIR . 'templates/');
-
-/*
-// HTML拡張子(*)
-*/
-define( 'BOOT_HTML_EXTENSION', '.html');
-
-/*
-// ファイルパス調整用(*)
-*/
-define( 'BOOT_DELETE_PATH', 'testplay/denryoku-sharing/html/');
-
-/*
-// オリジナルURL(*)
-*/
-define( 'BOOT_ORIGIN_PARAM', 'origin_file');
+// BOOTSTRAP設定
+require 'bootstrap-config.inc.php';
+// コード取得
+require_once WEB_APP_CONFIG_DIR.'code.inc.php';
 
 
 
@@ -69,27 +20,34 @@ if ($_GET[BOOT_ORIGIN_PARAM]) {
 if (substr($bootPhpFile, -1) == "/") $bootPhpFile.= "index";
 if (substr($bootHtmlFile, -1) == "/") $bootHtmlFile.= "index";
 
-define( 'BOOT_PHP_FILE', substr($bootPhpFile, 1).BOOT_PHP_EXTENSION);
-define( 'BOOT_HTML_FILE', substr($bootHtmlFile, 1).BOOT_HTML_EXTENSION);
+define( 'BOOT_PHP_FILE', substr($bootPhpFile, 1));
+define( 'BOOT_HTML_FILE', substr($bootHtmlFile, 1));
 
+// 静的コンテンツ
 if (strpos($bootHtmlFile, '.') !== false && substr($bootHtmlFile, (strlen($bootHtmlFile) - strlen(BOOT_PHP_EXTENSION)), strlen(BOOT_PHP_EXTENSION)) != BOOT_PHP_EXTENSION) {
-	if (file_exists(BOOT_HTML_DIR.$bootHtmlFile)) {
-		require_once BOOT_HTML_DIR.$bootHtmlFile;
+	$filePathInfo = pathinfo($bootHtmlFile);
+	if (array_search($filePathInfo['extension'], $FILE_EXTENSION_CODE) !== false) {
+		$mime = array_search($filePathInfo['extension'], $FILE_EXTENSION_CODE);
+		header('Content-Type: '.$mime);
+		require_once BOOT_HTML_DIR.BOOT_HTML_FILE;
 		exit;
 	}
+	header("HTTP/1.0 404 Not Found");
+	require 'page/404.html';
+	exit;
 }
 
-if (file_exists(BOOT_PHP_DIR.BOOT_PHP_FILE)) {
+if (file_exists(BOOT_PHP_DIR.BOOT_PHP_FILE.BOOT_PHP_EXTENSION)) {
 	# PHPロジックあり
 	require_once WEB_APP_CONFIG_DIR."cfg.inc.php";
-	require_once BOOT_PHP_DIR.BOOT_PHP_FILE;
-	if (file_exists(BOOT_HTML_DIR.BOOT_HTML_FILE)) require_once BOOT_HTML_DIR.BOOT_HTML_FILE;
+	require_once BOOT_PHP_DIR.BOOT_PHP_FILE.BOOT_PHP_EXTENSION;
+	if (file_exists(BOOT_HTML_DIR.BOOT_HTML_FILE.BOOT_HTML_EXTENSION)) require_once BOOT_HTML_DIR.BOOT_HTML_FILE.BOOT_HTML_EXTENSION;
 	exit;
-} elseif (file_exists(BOOT_HTML_DIR.BOOT_HTML_FILE)) {
+} elseif (file_exists(BOOT_HTML_DIR.BOOT_HTML_FILE.BOOT_HTML_EXTENSION)) {
 	# HTMLビューあり
 	require_once WEB_APP_CONFIG_DIR."cfg.inc.php";
 	require_once WEB_APP."public.php";
-	require_once BOOT_HTML_DIR.BOOT_HTML_FILE;
+	require_once BOOT_HTML_DIR.BOOT_HTML_FILE.BOOT_HTML_EXTENSION;
 	exit;
 }
 
